d229909ccb1d3f3bb1fdb1e21ba53b67c0f5ae3a
13566wu@gmail.com <13566wu@gmail.com>
2026-02-22 00:42:04 +0530

    first commit

---
+++ src/main/java/com/example/Test5Application.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test5Application {
+
+	public static void main(String[] args) {
+		String name = "mike";
+		int x = 100;
+		boolean isPresent=false;
+		int y = 200;
+		char a = 'a';
+		int y = 200;
+		double z = 10.3;
+		boolean isPresent=false;
+		float m = 10.3f;
+		SpringApplication.run(Test5Application.class, args);
+	}
+
+}
